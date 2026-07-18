6722b8a791c89add438ba543417815ea9d9fdb0c
ma-in-ko <maiko.nakao.18@gmail.com>
2026-07-18 18:09:17 +0900

    認証機能（一般ユーザー）のphpUnitテストを実装

---
+++ src/tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function 名前が未入力の場合バリデーションメッセージが表示される()
+    {
+        $response =$this->post('/register', [
+            'name' => '',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+
+        ]);
+
+
+        $response->assertSessionHasErrors([
+            'name'=> 'お名前を入力してください',
+            ]);
+    }
+
+    /** @test */
+    public function メールアドレスが未入力の場合バリデーションメッセージが表示される()
+    {
+        $response = $this->post('/register', [
+            'name' => 'テスト太郎',
+            'email' => '',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+
+        ]);
+
+
+        $response->assertSessionHasErrors([
+            'email' => 'メールアドレスを入力してください',
+        ]);
+    }
+
+    /** @test */
+    public function パスワードが8文字未満の場合バリデーションメッセージが表示される()
+    {
+        $response = $this->post('/register', [
+            'name' => 'テスト太郎',
+            'email' => 'test@example.com',
+            'password' => 'pass123',
+            'password_confirmation' => 'pass123',
+
+        ]);
+
+
+        $response->assertSessionHasErrors([
+            'password' => 'パスワードは8文字以上で入力してください',
+        ]);
+    }
+
+    /** @test */
+    public function パスワードが一致しない場合バリデーションメッセージが表示される()
+    {
+        $response = $this->post('/register', [
+            'name' => 'テスト太郎',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'passward',
+
+        ]);
+
+
+        $response->assertSessionHasErrors([
+            'password' => 'パスワードと一致しません',
+        ]);
+    }
+
+    /** @test */
+    public function パスワードが未入力の場合バリデーションメッセージが表示される()
+    {
+        $response = $this->post('/register', [
+            'name' => 'テスト太郎',
+            'email' => 'test@example.com',
+            'password' => '',
+            'password_confirmation' => 'password',
+
+        ]);
+
+
+        $response->assertSessionHasErrors([
+            'password' => 'パスワードを入力してください',
+        ]);
+    }
+
+    /** @test */
+    public function フォームに内容が入力されていた場合データが正常に保存される()
+    {
+        $response = $this->post('/register', [
+            'name' => 'テスト太郎',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'テスト太郎',
+            'email' => 'test@example.com',
+        ]);
+
+        $response->assertRedirect('/attendance');
+    }
+
+}
